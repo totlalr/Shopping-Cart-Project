97b98823bd52f5bb0dc1a666d8914aa79e873bff
Armin Vaghef <armin88vaghef@gmail.com>
2023-01-10 16:01:13 +0330

    add context and dispatcher for product add remove and ...

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 // Functions
-import { shorten } from "../../helper/function";
+import { shorten, isInCart, quantityCount } from "../../helper/function";
+
+//Context
+import { cardContext } from "./../../context/CardContextProvider";
+
+//Icon
+import trashIcon from "../../assets/icons/trash.svg";
 
 const Product = ({ productData }) => {
+  const { state, dispatch } = useContext(cardContext);
+
   return (
     <div>
       <img src={productData.image} alt="product" style={{ width: "200px" }} />
@@ -12,7 +20,42 @@ const Product = ({ productData }) => {
       <div>
         <Link to={`/products/${productData.id}`}>Details</Link>
         <div>
-          <button>Add to Cart</button>
+          {quantityCount(state, productData.id) > 1 && (
+            <button
+              onClick={() =>
+                dispatch({ type: "DEREASE", payload: productData })
+              }
+            >
+              -
+            </button>
+          )}
+
+          {quantityCount(state, productData.id) === 1 && (
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: productData })
+              }
+            >
+              <img src={trashIcon} alt="trash" style={{ width: "20px" }} />
+            </button>
+          )}
+          {isInCart(state, productData.id) ? (
+            <button
+              onClick={() =>
+                dispatch({ type: "INCREASE", payload: productData })
+              }
+            >
+              +
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                dispatch({ type: "ADD_ITEM", payload: productData })
+              }
+            >
+              Add to Cart
+            </button>
+          )}
         </div>
       </div>
     </div>
